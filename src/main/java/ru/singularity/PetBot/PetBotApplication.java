ab2d87fd25f32618029a0e7adec7bb81a5d7610c
Med77_1 <mm7737333@gmail.com>
2025-02-14 11:59:30 +0300

    Start project Telegram Bot

---
+++ src/main/java/ru/singularity/PetBot/PetBotApplication.java
@@ -0,0 +1,13 @@
+package ru.singularity.PetBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetBotApplication.class, args);
+	}
+
+}
